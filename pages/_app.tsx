d5fcf4a9cad9a37954a8ef150f9386471e62d6da
Tony Jemba <jjembatony@gmail.com>
2022-07-22 23:47:43 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<h1>Api only works on localhost</h1>)
 }
 
 export default MyApp
